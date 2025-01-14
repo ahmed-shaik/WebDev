8249fe883653cf55fa354845b0cd45cfe1a9d0c0
ahmed-shaik <ahmedshaikvk18@gmail.com>
2025-01-14 19:07:20 +0530

    Completed JavaScript chapter 1

---
+++ js/part1.js
@@ -0,0 +1,54 @@
+// we are using console not vs code
+
+//js uses repel read-evaluate-print-loop
+
+//data types in js
+// Number, Boolean, String, Undefined, Null, Bigint, Symbol
+
+// a = 20;
+// typeof a;
+
+// Number in js
+// a=20 || b=-23 || c=4.5
+// a=0.999999999999=0.999999999999
+// a=0.9999999999999999999999999999999999999999999=1
+
+//NaN - not a number
+// 0/0 - NaN
+// NaN+1 = NaN
+// NaN-1 = NaN
+
+// operator precedence
+// ()
+// **
+// *,/,%
+// +,-
+
+//Assignment operators
+// =, +=, -=
+
+// unary opearors
+// ++, --
+
+// Identifier Rules
+// All JavaScript variables must be identified with unique names (identifiers).
+// Names can contain letters, digits, underscores, and dollar signs. (no space)
+// Names must begin with a letter.
+// Names can also begin with $ and _.
+// Names are case sensitive (y and Y are different variables).
+// Reserved words (like JavaScript keywords) CANNOT be used as names.
+
+//Boolean - true false
+
+//What is TypeScript?
+// Static Typed, where JS is dynamic typed.
+
+// String - "" or '' both are equal
+// str.length
+// "tony" + " " + "stark" = "tony stark"
+
+// undefined
+// A variable that has not been assigned a value is of type undefined.
+
+// null
+// The null value represents the intentional absence of any object value.
