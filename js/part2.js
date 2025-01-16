7984811577566e49872f618768e96d2ae6431f18
ahmed-shaik <ahmedshaikvk18@gmail.com>
2025-01-16 20:38:55 +0530

    completed javascript chapter 2

---
+++ js/part2.js
@@ -0,0 +1,53 @@
+// console.log();
+// name a js file app.js
+// console.log("Log Everything");
+// console.log("Log Everything");
+// let a = 10;
+// let b = 15;
+// console.log("Sum is :", a + b);
+
+//template literals    -  ``- back ticks
+// let pen = 10;
+// let pencil = 5;
+// // console.log("The totoal sum is :", pen + pencil, "Rupees");
+// console.log(`The totoal sum is : ${pen + pencil}`);
+
+//Arithmetic (+, -, *, /, %, **)
+//Unary(++, --)
+// Assignment (=, +=, -=, *=, /=, %= etc.)
+//Comparison == - igonores type, === - checks type
+// 0 == " " || null == undefined; // true
+//Logical
+// javascript uses unicode
+// 'a' - 61, 'b' - 62, 'c' - 63, 'd' - 64, 'e' - 65, 'f' - 66, 'g' - 67, 'h' - 68, 'i' - 69
+// 'A' - 41, 'B' - 42, 'C' - 43, 'D' - 44, 'E' - 45, 'F' - 46, 'G' - 47, 'H' - 48, 'I' - 49
+
+// conditional statements
+// let age = 30;
+// if (age > 18) {
+//   console.log("You can vote");
+// }
+// if(){}
+// else if (){}
+// else if (){}
+// else {}
+// let color = " red";
+// switch (color) {
+//   case "red":
+//     console.log("stop");
+//     break;
+//   case "yellow":
+//     console.log("slow down");
+//     break;
+//   case "green":
+//     console.log("Go");
+//     break;
+//   default:
+//     console.log("error");
+// }
+
+// alert("something is wrong!");
+// console.error("Where is the error");
+// console.warn();
+let a = prompt("What is your name lil buddy? ");
+console.log(a);
