a5d6fb35ccdbd635984e47174b22ac1a97eff498
ahmed-shaik <ahmedshaikvk18@gmail.com>
2025-01-17 18:47:14 +0530

    completed javascript chapter 3

---
+++ js/part3.js
@@ -0,0 +1,42 @@
+//string methods
+// let msg = "    Hello  ";
+// console.log(msg.trim());
+
+// let pass = prompt("Enter Password:");
+// console.log(pass.toUpperCase());
+// toLowerCase();
+
+// let str = "asefse";
+// console.log(str.indexOf("fse"));
+
+// slice(start, end)
+// let str = "ILoveCodingLove";
+// // console.log(str.replace("Love", "do"));
+
+// console.log(str.repeat(3));
+
+// Arrays - arrays are mutable objects
+// let names = ["Ahmed", "Pardhu", "vatraPrasad"];
+// console.log(names);
+
+// Array Methods
+// Push : add to end
+// Pop : delete from end & returns it
+// Unshift : add to start
+// Shift : delete from start & returns it
+// indexOf : index of first element in array
+// includdes : searches for a vavlue. returns true if found, false if not
+// concat: add two strings
+// str.concat("str");
+// reverse : reverse a string
+// slice : Smae as in strings
+
+// splice : removes I replaces I add elements in place
+// splice( start, deleteCount, item0...itemN)
+
+// cars = ["audit", "bmw", "xuv", "maruti"];
+// cars.splice(3); //ddelete last element
+// cars.splice(0, 1); //delete first element form 0 index
+// cars.splice(0, 0, "adfa", "aefdad", "afaf");
+
+// sort() - it can be used on strings or characters, not on numbers
