2118540efb9fd1187100d0487a7f3d07883c9025
ahmed-shaik <ahmedshaikvk18@gmail.com>
2025-01-19 19:40:40 +0530

    Completed javascript chapter 5

---
+++ js/part5.js
@@ -0,0 +1,80 @@
+// JS Objects Literals
+// Used to store keyed collections & complex entities.
+
+// property (key, value) pair
+// Objects are a collection of properties
+
+// const student = {
+//   name: "Ahmed",
+//   age: 19,
+//   marks: 100,
+// };
+// // console.log(student);
+// // console.log(student["name"]);
+// // console.log(student.age);
+// student.name = "AAA";
+// student.gender = "male";
+// delete student.marks;
+// console.log(student);
+
+// const classInfo = {
+//   ahmed: {
+//     grade: "A",
+//     city: "vizag",
+//   },
+//   shradha: {
+//     grade: "O",
+//     city: "vizag",
+//   },
+//   aman: {
+//     grade: "V",
+//     city: "vizag",
+//   },
+// };
+// console.log(classInfo);
+// console.log(classInfo.ahmed);
+// console.log(classInfo.ahmed["grade"]);
+// console.log(classInfo.aman);
+// console.log(classInfo.shradha);
+
+// Array of Objects
+// Storing information of multiple students
+// classInfo = [
+//   {
+//     name: "ahmed",
+//     grade: "A",
+//     city: "vizag",
+//   },
+//   {
+//     name: "shradha",
+//     grade: "V",
+//     city: "vizag",
+//   },
+//   {
+//     name: "aman",
+//     grade: "V",
+//     city: "vizag",
+//   },
+// ];
+// console.log(classInfo);
+// console.log(classlnfo[1].grade);
+
+// Math Object
+// Properties
+// Math.Pl
+// Math.E
+
+// Methods
+// Math.abs(n)
+// Math.pow(a,b)
+// Math.floor(n)
+// Math.ceil(n)
+// Math.random()
+
+// number between 1 to 10
+rand = Math.floor(Math.random() * 10) + 1;
+console.log(rand);
+
+// number between 101 to 110
+rand = Math.floor(Math.random() * 10) + 101;
+console.log(rand);
