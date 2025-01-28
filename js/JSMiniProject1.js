ded17e9cf0b53600ad43ff7dfed91c2fa7c9fcd4
ahmed-shaik <ahmedshaikvk18@gmail.com>
2025-01-28 19:51:48 +0530

    completed javascript project

--- js/JSMiniProject1.js
+++ js/JSMiniProject1.js
@@ -56,6 +56,7 @@ function btnFlash(btn) {
   }, 250);
 }
 function levelUp() {
+  userSeq = [];
   l++;
   h2.innerText = `level ${l}`;
   let randIdx = Math.floor(Math.random() * 3);
@@ -65,12 +66,37 @@ function levelUp() {
   btnFlash(randBtn);
 }
 
+function checkAns(idx) {
+  if (userSeq[idx] == gameSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerText = "Game over!, Press any key to restart";
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function () {
+      document.querySelector("body").style.backgroundColor = "white";
+    }, 150);
+    reset();
+  }
+}
+
 function btnPress() {
   let btn = this;
   btnFlash(btn);
+  userColor = btn.getAttribute("id");
+  userSeq.push(userColor);
+  checkAns(userSeq.length - 1);
 }
 
 let allbtns = document.querySelectorAll(".btn");
 for (btn of allbtns) {
   btn.addEventListener("click", btnPress);
+}
+
+function reset() {
+  start = false;
+  gameSeq = [];
+  userSeq = [];
+  l = 0;
 }
