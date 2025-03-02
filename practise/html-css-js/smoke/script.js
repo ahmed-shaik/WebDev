7326d940d8d8ca65422d05913b504f2a828b309d
Ahmed <ahmedshaikvk18@gmail.com>
2025-03-02 11:48:37 +0530

    completed smoke animation project

---
+++ practise/html-css-js/smoke/script.js
@@ -0,0 +1,8 @@
+const text = document.querySelector("#text");
+text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+const alphaSpan = document.querySelectorAll("span");
+alphaSpan.forEach((span) => {
+  span.addEventListener("mouseover", () => {
+    span.classList.add("active");
+  });
+});
