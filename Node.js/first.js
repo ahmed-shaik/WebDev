de6687804acc4320c446b1f7a0ea787dae2679c8
Ahmed <ahmedshaikvk18@gmail.com>
2025-03-25 20:58:55 +0530

    started node js and completed till module.exports

---
+++ Node.js/first.js
@@ -0,0 +1,14 @@
+// JavaScript Runtime Environment
+// It is used for server side programming.
+// *Node.js is not a language, library or framework.
+
+// it is used to run js outside the browser
+
+// node fileName.js
+
+// process : This object provides information about, and control over, the current Node.js process.
+// process.argv : returns an array containing the command-line arguments passed when the Node.js process was launched.
+
+// module.exports - used to use the functions from the other files
+const val = require("./for(first)usageofmodulexportex-1");
+console.log(val);
---
+++ Node.js/for(first)usageofmodulexportex-1.js
@@ -0,0 +1,12 @@
+// this file is used ftfor example for the module.exports in first file.
+const sum = (a, b) => {
+  return a + b;
+};
+const diff = (a, b) => {
+  return a - b;
+};
+
+const g = 10;
+const PI = 3.14;
+
+module.exports = 11222;
