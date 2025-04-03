59c0bf81af7da666384c036b81062df5a2b7cd87
Ahmed <ahmedshaikvk18@gmail.com>
2025-04-03 20:21:01 +0530

    completed quora restful app

--- Express.js/4rest/index.js
+++ Express.js/4rest/index.js
@@ -19,6 +19,7 @@
 const express = require("express");
 const path = require("path");
 const { v4: uuid4 } = require("uuid");
+const methodOverride = require("method-override");
 const app = express();
 const port = 8080;
 
@@ -27,6 +28,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true })); // to parse form data
+app.use(methodOverride("_method"));
 
 let posts = [
   { id: uuid4(), username: "ahmed", content: "This is post 1" },
@@ -70,12 +72,20 @@ app.patch("/posts/:id", (req, res) => {
   let con = req.body.content;
   let post = posts.find((p) => id == p.id);
   post.content = con;
+  res.redirect("/posts");
 });
 
 app.get("/posts/:id/edit", (req, res) => {
   let { id } = req.params;
   let post = posts.find((p) => id == p.id);
-  res.render("edit.ejs", { posts });
+  console.log(post);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
 });
 
 app.listen(port, () => {
