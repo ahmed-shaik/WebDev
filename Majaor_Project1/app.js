4b7e85eadc1a8d25f12b82fbffec5a9b4bc2aae5
Ahmed <ahmedshaikvk18@gmail.com>
2025-05-29 19:39:54 +0530

    completed project phase1 parta

--- Majaor_Project1/app.js
+++ Majaor_Project1/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/Listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 main()
   .then(() => {
@@ -49,9 +51,52 @@ app.get("/", (req, res) => {
 //   res.send(slisting);
 // });
 
+// index route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("./listings/index.ejs", { allListings });
+});
+
+// new route
+app.get("/listings/new", (req, res) => {
+  res.render("./listings/new.ejs");
+});
+
+//create route
+app.post("/listings", async (req, res) => {
+  const listing = req.body.listing;
+  // console.log(listing);
+  const newListing = await new Listing(listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+// edit route
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+});
+
+// update route
+app.patch("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+// Destroy route
+app.delete("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
+
+// show route
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/show.ejs", { listing });
 });
 
 app.listen(8080, () => {
